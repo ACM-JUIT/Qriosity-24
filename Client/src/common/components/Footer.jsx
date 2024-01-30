af65464054c94d2f91bf6766c39c8b5abfaf4ee5
Naman Tomar <namantomar1453@gmail.com>
2024-01-30 17:51:09 +0530

    About Us and Rules Refractored

--- Client/src/common/components/Footer.jsx
+++ Client/src/common/components/Footer.jsx
@@ -1,9 +1,9 @@
-import bgimg from '../../../public/download.png';
+
 function Footer() {
   return (
     <div className="font-montserrat">
       <footer>
-        <img src={bgimg} alt="" className='h-28 pt-8 pr-8 mx-auto'/>
+        <h1 className="text-white">blah balh</h1>
     </footer>
       </div>
   )
